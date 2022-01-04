9d3f070aacb4269da4f4c3d5a495bb7a516ca294
Luis Bermudez <lapbermudez@gmail.com>
2022-01-03 21:33:24 -0600

    Routes updated, login logic

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -112,7 +112,7 @@ router.post("/login", async (req, res, next) => {
 router.post('/logout', (req, res, next) => {
     req.session.destroy(err => {
         if(err) next(err);
-        res.redirect('/landpage');
+        res.redirect('/landing');
     })
 })
 
