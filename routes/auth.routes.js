46847df4681ae572eb59e18f93193c515c692f75
EvaAlonzo <la_rana9@hotmail.com>
2022-01-03 18:02:40 -0600

    some changes, test

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs');
 const saltRounds = 10;
 const mongoose = require('mongoose');
 
+router.get("/landing", (req,res,next) => res.render("auth/landing"));
 router.get("/signup", (req, res, next) => res.render("auth/signup"));
 
 router.post("/signup", async (req, res, next) => {
