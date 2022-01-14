387df1b4acbbfb5db151bf7e3f3b0ecf5d01a073
Luis Bermudez <lapbermudez@gmail.com>
2022-01-14 09:15:18 -0600

    Send google creds to .env file for security reasons

--- app.js
+++ app.js
@@ -67,9 +67,8 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "947016374633-kaqcu4bjt91efabcmp2rssfmscjesbf6.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientID: process.env.GOOGLE_CLIENT,
+      clientSecret: process.env.GOOGLE_SECRET,
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
